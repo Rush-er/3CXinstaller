6770501bd2fa28c03bc471d8d5c515bbad356ef7
Stocco Matis <32925370+Rush-er@users.noreply.github.com>
2020-09-18 09:16:37 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,7 @@
+cd ~/
+apt install sudo
+wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add
+echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
+sudo apt update
+sudo apt install net-tools dphys-swapfile
+sudo apt install 3cxpbx
