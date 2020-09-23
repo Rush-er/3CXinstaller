28a930313aef44089ee353171a91e85ce0fc9024
Stocco Matis <32925370+Rush-er@users.noreply.github.com>
2020-09-23 12:56:05 +0200

    New installation order
    This fix the problem with 3CX setup info on screen after the installation

--- installer.sh
+++ installer.sh
@@ -3,6 +3,7 @@ apt install sudo
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add
 echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
 sudo apt update
+sudo apt install open-vm-tools
 sudo apt install net-tools dphys-swapfile
 sudo apt install 3cxpbx
-sudo apt install open-vm-tools
+
