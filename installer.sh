d7dffd50ba74c3aca4dc79999bdab17c07fd57e4
Stocco Matis <32925370+Rush-er@users.noreply.github.com>
2022-03-07 10:41:42 +0100

    Bump 3cx version to v18.x
    Debian 10 (buster) = v18
    Debian 9 (stretch)  = v16

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 cd ~/
 apt install sudo
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add
-echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
+echo "deb http://downloads-global.3cx.com/downloads/debian buster main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
 sudo apt update
 sudo apt install open-vm-tools
 sudo apt install net-tools dphys-swapfile
