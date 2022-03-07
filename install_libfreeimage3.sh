73cc9721b368dffab2c9afcd7fdeee85b41a3f74
Stocco Matis <32925370+Rush-er@users.noreply.github.com>
2022-03-07 10:59:54 +0100

    Fix installer

--- install_libfreeimage3.sh
+++ install_libfreeimage3.sh
@@ -1,4 +1,4 @@
-apt-get update
-apt-get install libilmbase12
-wget http://security.debian.org/debian-security/pool/updates/main/o/openexr/libopenexr22_2.2.0-11+deb9u4_amd64.deb
+apt-get update;
+apt-get install libilmbase12 ||
+wget http://security.debian.org/debian-security/pool/updates/main/o/openexr/libopenexr22_2.2.0-11+deb9u4_amd64.deb && 
 dpkg -i libopenexr22_2.2.0-11+deb9u4_amd64.deb
