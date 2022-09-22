7f5ea728708589f6aba8e77530b0938542121bb6
Stocco Matis <32925370+Rush-er@users.noreply.github.com>
2022-09-22 10:07:12 +0200

    Fix missing gnupg2 on Debian buster

--- installer.sh
+++ installer.sh
@@ -1,5 +1,6 @@
 cd ~/
 apt install sudo
+sudo apt install gnupg2
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add
 echo "deb http://downloads-global.3cx.com/downloads/debian buster main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
 sudo apt update
