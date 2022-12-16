87bd736800babe747241e35ff54184c2a6ed9460
Stocco Matis <32925370+Rush-er@users.noreply.github.com>
2022-12-16 12:54:34 +0100

    Update debian11_installer.sh

--- debian11_installer.sh
+++ debian11_installer.sh
@@ -3,7 +3,7 @@ cd ~/
 sudo apt install apt-transport-https -y
 sudo apt install gpg -y
 sudo apt install net-tools dphys-swapfile -y
-wget -O- https://repo.3cx.com/key.pub | gpg --dearmor | sudo tee /usr/share/keyrings/3cx-archive-keyring.gpg > /dev/null
-/bin/echo "deb [arch=amd64 signed-by=/usr/share/keyrings/3cx-archive-keyring.gpg] http://repo.3cx.com/3cx bullseye main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
+wget -O- https://repo.3cx.com/key.pub | sudo apt-key add
+echo "deb http://repo.3cx.com/3cx bullseye main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
 sudo apt update
 sudo apt install 3cxpbx
