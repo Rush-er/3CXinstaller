b868c4ca16a0965df554a063a7257e809121e282
Stocco Matis <32925370+Rush-er@users.noreply.github.com>
2022-12-16 13:01:58 +0100

    Update debian11_installer.sh

--- debian11_installer.sh
+++ debian11_installer.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cd ~/
-sudo apt install apt-transport-https -y
+wget -O- https://repo.3cx.com/key.pub | sudo apt-key add
+echo "deb http://repo.3cx.com/3cx bullseye main" | sudo tee /etc/apt/sources.list.d/3cxpbx.listsudo apt install apt-transport-https -y
+sudo apt update
 sudo apt install gpg -y
 sudo apt install net-tools -y
-wget -O- https://repo.3cx.com/key.pub | sudo apt-key add
-echo "deb http://repo.3cx.com/3cx bullseye main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
-sudo apt update
+sudo apt install nginx -y
+rm -f /etc/nginx/sites-enabled/default
+sudo systemctl reload nginx
 sudo apt install 3cxpbx
