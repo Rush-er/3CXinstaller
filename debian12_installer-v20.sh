ad1e1c9a34816a602735ebb967f556a775a21051
Matis <32925370+Rush-er@users.noreply.github.com>
2024-05-14 11:57:25 +0200

    Update debian12_installer-v20.sh

--- debian12_installer-v20.sh
+++ debian12_installer-v20.sh
@@ -2,9 +2,9 @@
 cd ~/
 apt install jq curl wget sudo apt-transport-https gpg net-tools -y
 wget -O- http://repo.3cx.com/key.pub | gpg --dearmor | sudo tee /usr/share/keyrings/3cx-archive-keyring.gpg > /dev/null
-/bin/echo "deb [arch=amd64 by-hash=yes signed-by=/usr/share/keyrings/3cx-archive-keyring.gpg] http://repo.3cx.com/debian/2000 bookworm main"  	>> /etc/apt/sources.list
+/bin/echo "deb [arch=amd64 by-hash=yes signed-by=/usr/share/keyrings/3cx-archive-keyring.gpg] http://repo.3cx.com/debian/2000 bookworm main" >> /etc/apt/sources.list
 /bin/echo "deb [arch=amd64 by-hash=yes signed-by=/usr/share/keyrings/3cx-archive-keyring.gpg] http://repo.3cx.com/debian-security/2000 bookworm-security main" >> /etc/apt/sources.list
-/bin/echo "deb [arch=amd64 by-hash=yes signed-by=/usr/share/keyrings/3cx-archive-keyring.gpg] http://repo.3cx.com/3cx bookworm main"  			> /etc/apt/sources.list.d/3cxpbx.list
+/bin/echo "deb [arch=amd64 by-hash=yes signed-by=/usr/share/keyrings/3cx-archive-keyring.gpg] http://repo.3cx.com/3cx bookworm main" > /etc/apt/sources.list.d/3cxpbx.list
 apt update
 apt install postgresql-15 postgresql-client-15 nginx -y
 apt-cache policy 3cxpbx | grep -o '20.*' | grep -o '^\S*'
